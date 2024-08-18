82dfb89c67631cd92aa0b68b647f451e9b46796e
Czarina Jane Arellano <czarinaarellano12@gmail.com>
2024-08-19 01:16:25 +0800

    liked posts content
    hehe sorry kung makakagulo sample data sa ts pero yes amsori

--- src/app/main/components/likedposts/likedposts.component.ts
+++ src/app/main/components/likedposts/likedposts.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrl: './likedposts.component.scss'
 })
 export class LikedpostsComponent {
+  items: Item[] = [
+    {
+      title: 'Newest Article',
+      category: 'Technology',
+      author: 'John Doe',
+      date: '2024-08-18',
+      image: '../../../../assets/images/NoImage.png',
+      description: 'This is the description for the newest article. This is the description for the newest article. This is the description for the newest article. This is the description for the newest article. This is the description for the newest article. This is the description for the newest article.'
+    },
+    {
+      title: 'Sample Title of Posted Idea One Two Three Four',
+      category: 'Science',
+      author: 'Jane Smith',
+      date: '2024-08-17',
+      image: '../../../../assets/images/ResdeApp.PNG',
+      description: 'This is the description for another article.'
+    },
+    {
+      title: 'Sample Title of Posted Idea One Two Three Four Five Six Seven',
+      category: 'Science',
+      author: 'Jane Smith',
+      date: '2024-08-17',
+      image: '../../../../assets/images/clipart.png',
+      description: 'This is the description for another article.'
+    },
+    // Add more items as needed
+  ];
 
+  selectedItem: Item = this.items[0];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  selectItem(item: Item): void {
+    this.selectedItem = item;
+  }
+
+}
+
+interface Item {
+  title: string;
+  category: string;
+  author: string;
+  date: string;
+  image: string;
+  description: string;
 }
--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LikedpostsComponent } from './components/likedposts/likedposts.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./components/homepage/homepage.module').then(m => m.HomepageModule) },
+  { path: 'likes', component: LikedpostsComponent },
   { path: 'uploads', loadChildren: () => import('./components/uploads/uploads.module').then(m => m.UploadsModule) }
 ];
 
