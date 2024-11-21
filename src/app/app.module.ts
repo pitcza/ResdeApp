e7824391bc77d48510a55a880daa9eccbb8af1e9
Jay-vee <131955010+JAY-VEE85@users.noreply.github.com>
2024-11-22 04:04:32 +0800

    PWA
    to be able the user to download the site smth like that

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +16,7 @@ import { AdminComponent } from './admin/admin.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AgriExpertComponent } from './agri-expert/agri-expert.component';
 import { AgriExpertModule } from './agri-expert/agri-expert.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,13 @@ import { AgriExpertModule } from './agri-expert/agri-expert.module';
     MaterialModule,
     AdminModule,
     AgriExpertModule,
-    HttpClientModule
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     provideClientHydration(),
